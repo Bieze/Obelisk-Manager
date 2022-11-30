c615bb0825e8e4522e609021d441e9429d2b54a3
abdulh4ni <abdulh4ni@icloud.com>
2022-11-29 23:18:09 -0600

    Should compile correctly now

--- src/SettingsLocation.hpp
+++ src/SettingsLocation.hpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License along with Obe
 #include "base64.hpp"
 #include "takeUserPassword.hpp"
 #include <boost/filesystem.hpp>
-#include <SQLiteCpp/SQLiteCpp.h>
+#include "SQLiteCpp/SQLiteCpp.h"
 #include <boost/filesystem/fstream.hpp>
 
 using namespace std;
